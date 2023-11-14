d8d1bde5dad52d8cfd6647e0a68e89d8a104c3ae
KatAshLey <charmed.kat@gmail.com>
2023-11-14 14:39:59 +1300

    sort files

--- Week03/pydantic/dto/Author.py
+++ Week03/pydantic/dto/Author.py
@@ -15,6 +15,6 @@ class Author(BaseModel):
     @classmethod
     def check_valid_author_id(cls, author_id: str):
         check_id=re.search(r"[A-Z]{4}-[0-9]{4}", author_id)
-        return check_id != None
+        return check_id != None and len(author_id) == 9
         """ assert author_id[0:4].isalpha() and author_id[-4:].isnumeric(), "Author Id is not XXXX-YYYY, X is capital letter, and Y is number"
         return author_id """
